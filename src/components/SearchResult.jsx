84a6899e5f547c7f9e83bc8a2980a48363098b7f
Koushik Siva <koushiksiva9@gmail.com>
2024-03-12 14:34:11 -0500

    Fixed issues in search page

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import {  useSelector } from "react-redux";
 function SearchResult() {
   const userSearch = useSelector(state => state.userSearchStore ?? null);
-  if(userSearch === null){
+
+  if(userSearch.disease === ""){
     return (
       <div class="results">
         <div class="text-center m-5">
@@ -26,34 +27,34 @@ function SearchResult() {
           </blockquote>
           
           <div class="text-center">
-              <h3><strong>Disease :</strong> {userSearch.disease}</h3>
+              <h3><strong>Disease :</strong> { userSearch.disease}</h3>
               <br></br>
               <h5><strong>Department to Consult:</strong>{userSearch.doctorDepartment}</h5>
               <br/>
               <h5><strong>Medications:</strong></h5>
               <ul className="list-group">
-              {userSearch.medications.map((M, index) =>
+              {userSearch.medications && userSearch.medications.map((M, index) =>
                 <li className="list-group-item" key={index}>{M}</li>
                 )}
               </ul>
               <br/>
               <h5><strong>Treatments:</strong></h5>
               <ul className="list-group">
-              {userSearch.treatments.map((M, index) =>
+              {userSearch.treatments && userSearch.treatments.map((M, index) =>
                 <li className="list-group-item" key={index}>{M}</li>
                 )}
               </ul>
               <br/>
               <h5><strong>Home Remedies:</strong></h5>
               <ul className="list-group">
-              {userSearch.homeRemedies.map((M, index) =>
+              {userSearch.homeRemedies && userSearch.homeRemedies.map((M, index) =>
                 <li className="list-group-item" key={index}>{M}</li>
                 )}
               </ul>
               <br/>
               <h5><strong>Prevention Measures:</strong></h5>
               <ul className="list-group">
-              {userSearch.preventionMeasures.map((M, index) =>
+              {userSearch.preventionMeasures && userSearch.preventionMeasures.map((M, index) =>
                 <li className="list-group-item" key={index}>{M}</li>
                 )}
               </ul>
